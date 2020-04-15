53abdce90fdde01c3f96016f6535301938307d44
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-15 22:00:50 +0200

    added styles

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 
 const ImageLinkForm = () => {
   return (
     <div>
-      <h1>ImageLinkForm</h1>
-    </div>
+      <p className='text f4'>
+        {'This Magic Brain will detect faces in your pictures. Try it!'}
+      </p>
+      <div className="center">
+          <div className="center form pa4 br3 shadow-5">
+            <input className='f4 pa2 w-70 center' type='tex'/>
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+          </div>
+        </div>
+      </div>
   )
 }
 
