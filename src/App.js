45b2da936c6cd87dba16fdb9fce216a6434b1502
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-15 22:15:28 +0200

    Added rank component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './index.css';
 
-import { Navigation, Logo, ImageLinkForm} from './components';
+import { Navigation, Logo, ImageLinkForm, Rank } from './components';
 
 class App extends Component {
   render() {
@@ -9,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 export { default as Logo } from './Logo/Logo';
 export { default as Navigation } from './Navigation/Navigation';
 export { default as ImageLinkForm } from './ImageLinkForm/ImageLinkForm';
+export { default as Rank } from './Rank/Rank';
