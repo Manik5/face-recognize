90c46f422e9fcce228ecda928dad4a9ddc6263f0
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-16 15:18:56 +0200

    added clarifai api key

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
 import Particles from "react-particles-js";
+import Clarifai from 'clarifai';
 import './App.css';
 import './index.css';
 
 import { Navigation, Logo, ImageLinkForm, Rank } from './components';
 
+
+const app = new Clarifai.App({
+  apiKey: "466bfdf6cf984671b3eb1c475c3cf957",
+});
+
+
 const particleOptions  = {
   particles: {
-    line_linked: {
-      shadow: {
+    number: {
+      value: 2500,
+      density: {
         enable: true,
-        color: "#3CA9D1",
-        blur: 5
+        value_area: 5000,
       }
     }
   }
 }
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response)
+          // do something with response
+        },
+        function (err) {
+          // there was an error
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,7 +61,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
       </div>
     );
   }
